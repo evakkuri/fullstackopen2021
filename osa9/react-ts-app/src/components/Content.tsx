73f685b2273b5cb3df329c72044c6a3b68f85eef
Elias Vakkuri <elias.vakkuri@iki.fi>
2021-08-03 19:31:13 +0300

    Tehtävä 9.15

--- osa9/react-ts-app/src/components/Content.tsx
+++ osa9/react-ts-app/src/components/Content.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { CoursePart, CourseParts } from "../types";
+import { assertNever } from "../utils";
 
-const ContentItem = ({ name, exerciseCount }: CoursePart) => {
+const Part = (part: CoursePart) => {
+
+  const renderSwitch = (part: CoursePart) => {
+    switch (part.type) {
+      case "normal":
+        return <i>{part.description}</i>
+      case "groupProject":
+        return <span>Number of group exercises {part.groupProjectCount}</span>
+      case "submission":
+        return (
+          <span>
+            <i>{part.description}</i><br/>
+            <span>Submit to: {part.exerciseSubmissionLink}</span>
+          </span>
+        )
+      case "special":
+        return <span>Required skills: {part.requirements.join(', ')}</span>
+      default:
+        return assertNever(part)
+    }
+  }
+
   return (
     <div>
       <p>
-        {name} {exerciseCount}
+        <b>{part.name} {part.exerciseCount}</b><br />
+        {renderSwitch(part)}
       </p>
     </div>
   )
@@ -14,7 +37,7 @@ const ContentItem = ({ name, exerciseCount }: CoursePart) => {
 const Content = ({ courseParts }: CourseParts) => {
   return (
     <div>
-      {courseParts.map(part => ContentItem(part))}
+      {courseParts.map(part => Part(part))}
     </div>
   )
 }
