7ab73de8d863cea92b252b49be60f913e9e08dbf
Elias Vakkuri <elias.vakkuri@iki.fi>
2021-08-08 10:09:31 +0300

    Tehtävä 8.4

--- osa8/library-backend/src/index.js
+++ osa8/library-backend/src/index.js
@@ -109,7 +109,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -118,7 +118,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (parent, args) => {
+      if (!args.author) books
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {
