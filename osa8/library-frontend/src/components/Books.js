70495a4a936e837a7e494040dec5fe94e9b179f8
Elias Vakkuri <elias.vakkuri@iki.fi>
2021-08-12 19:07:05 +0300

    Tehtävä 8.9

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,37 +1,49 @@
-
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+import { Table } from 'semantic-ui-react'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks  {
+      title
+      author
+      published
+    }
+  }
+`
 
 const Books = (props) => {
+  const books = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (books.loading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>
-      <h2>books</h2>
-
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
+      <h2>Books</h2>
+      <Table celled padded striped>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Title</Table.HeaderCell>
+            <Table.HeaderCell>Author</Table.HeaderCell>
+            <Table.HeaderCell>Published</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {books.data.allBooks.map(a =>
+            <Table.Row key={a.title}>
+              <Table.Cell>{a.title}</Table.Cell>
+              <Table.Cell>{a.author}</Table.Cell>
+              <Table.Cell>{a.published}</Table.Cell>
+            </Table.Row>
           )}
-        </tbody>
-      </table>
+        </Table.Body>
+      </Table>
     </div>
   )
 }
